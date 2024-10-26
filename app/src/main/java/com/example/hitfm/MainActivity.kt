3506b372c01c459a41047cfd29f02c7193d572f8
QuvonchbekGafurov <quvonchbekgafurov07@gmail.com>
2024-10-26 12:58:24 +0500

    finish

--- app/src/main/java/com/example/hitfm/MainActivity.kt
+++ app/src/main/java/com/example/hitfm/MainActivity.kt
@@ -4,18 +4,17 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import com.example.hitfm.ui.theme.HITFMTheme
 import com.example.hitfm.uiview.MainScreen
-import com.example.hitfm.uiview.RadioPlayer
-import java.lang.reflect.Modifier
 
 class MainActivity : ComponentActivity() {
 
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             HITFMTheme {
                 MainScreen()
